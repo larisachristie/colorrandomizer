1a4ab0780eae40f0787b24d9c54b6e035e70c6f6
Spencer Rudnick <spencer@synth.kitchen>
2020-02-10 23:01:33 +0100

    Clean up CRA junk

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class App extends React.Component {
+  state = {
+    // you decide what to put here
+  };
+
+  handleClick = () => {
+
+  };
+
+  render() {
+    return (
+    );
+  }
+}
+
+export default App;
